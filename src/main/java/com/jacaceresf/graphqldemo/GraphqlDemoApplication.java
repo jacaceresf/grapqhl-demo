4af153eab374537a01908955cf54fe92199f4ce9
jorgecac <jorge97caceres@gmail.com>
2021-07-31 14:24:26 -0400

    Query and data mutation are working.

--- src/main/java/com/jacaceresf/graphqldemo/GraphqlDemoApplication.java
+++ src/main/java/com/jacaceresf/graphqldemo/GraphqlDemoApplication.java
@@ -1,13 +1,40 @@
 package com.jacaceresf.graphqldemo;
 
+import com.jacaceresf.graphqldemo.beans.Food;
+import com.jacaceresf.graphqldemo.services.FoodService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class GraphqlDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GraphqlDemoApplication.class, args);
-	}
+    /**
+     * To initialize app with sample data.
+     *
+     * @param foodService
+     * @return
+     */
+    @Bean
+    ApplicationRunner init(FoodService foodService) {
+
+        return args -> {
+            Stream.of("Pizza", "Spam", "Eggs", "Avocado", "Burger").forEach(
+                    name -> {
+                        Food food = new Food();
+                        food.setName(name);
+                        foodService.saveFood(food);
+                    }
+            );
+            foodService.getFoods().forEach(System.out::println);
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlDemoApplication.class, args);
+    }
 
 }
--- src/main/java/com/jacaceresf/graphqldemo/services/FoodService.java
+++ src/main/java/com/jacaceresf/graphqldemo/services/FoodService.java
@@ -3,11 +3,17 @@ package com.jacaceresf.graphqldemo.services;
 
 import com.jacaceresf.graphqldemo.beans.Food;
 import com.jacaceresf.graphqldemo.repositories.FoodRepository;
+import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLContext;
+import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @GraphQLApi
@@ -15,12 +21,47 @@ public class FoodService {
 
     private final FoodRepository foodRepository;
 
+    private final List<String> goodFood = Arrays.asList("Avocado", "Spam");
+
     public FoodService(FoodRepository foodRepository) {
         this.foodRepository = foodRepository;
     }
 
+    /**
+     * Read all foods.
+     *
+     * @return All foods.
+     */
     @GraphQLQuery(name = "foods")
     public List<Food> getFoods() {
         return foodRepository.findAll();
     }
+
+    /**
+     * Read food by id.
+     *
+     * @param id
+     * @return
+     */
+    @GraphQLQuery(name = "food")
+    public Optional<Food> getFoodById(@GraphQLArgument(name = "id") Long id) {
+        return foodRepository.findById(id);
+    }
+
+    @GraphQLQuery(name = "isGood")
+    public boolean isGood(@GraphQLContext Food food) {
+        return goodFood.contains(food.getName());
+    }
+
+    /**
+     * Saves a food record.
+     *
+     * @param food
+     * @return
+     */
+    @GraphQLMutation(name = "saveFood")
+    public Food saveFood(@GraphQLArgument(name = "food") Food food) {
+        return foodRepository.save(food);
+    }
+
 }
